0470aefbd4755b8d4dd0c1d2bb3c85819b6c1481
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-05 16:47:01 -0700

    added annotations for IElement and Element

---
+++ ui/i_element.lua
@@ -0,0 +1,7 @@
+--- @meta
+--- Provides access to a UI element's properties.
+--- @class IElement
+--- @field IsVisible boolean `true` if this element is visible; otherwise, `false`.
+--- @field IsInteractable boolean `true` if this element is interactable; otherwise, `false`.
+--- @field IsTargetable boolean `true` if this element is targetable (i.e. mouse pointer blocking); otherwise, `false`.
+local IElement = {}
