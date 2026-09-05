e55190e533227532c2f645456a91af0989f03063
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-05 16:47:01 -0700

    added annotations for IElement and Element

---
+++ ui/element.lua
@@ -0,0 +1,5 @@
+--- @meta
+--- A UI element.
+--- @class Element : IElement, ISizeable, IRotatable
+--- @field IsVisible boolean `true` if this element is visible; otherwise, `false`.
+local Element = {}
