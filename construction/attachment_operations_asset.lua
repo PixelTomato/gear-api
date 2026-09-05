d275c111f7c783be6eb7a51ab212b1a6783d209c
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-05 01:27:45 -0700

    added annotations for AttachmentOperationsAsset

---
+++ construction/attachment_operations_asset.lua
@@ -0,0 +1,13 @@
+--- @meta
+--- An object exposing attachment operations.
+---
+--- Static methods are available via the "AttachmentOps" global.
+---
+--- Example:
+--- ```lua
+--- AttachmentOps.DeleteAllAttachments(construction)
+--- ```
+--- @class AttachmentOps : IAttachmentOperations
+AttachmentOps = {}
+
+--- @alias AttachmentOperationsAsset AttachmentOps
