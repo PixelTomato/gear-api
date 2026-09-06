75c6d089fc156e15911d5a474ad82055ef4b6d97
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-06 01:39:09 -0700

    added annotations for ILink, ILinker, and LinkerTool

---
+++ construction/ilink.lua
@@ -0,0 +1,6 @@
+--- @meta
+--- A link between two parts' link nodes, representing a logical connection between them.
+--- @class ILink : INullCheck
+--- @field LinkFromNode ILinkNode The link node on the source part.
+--- @field LinkToNode ILinkNode The link node on the destination part.
+local ILink = {}
