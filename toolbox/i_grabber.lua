9d469319c448d8b748130e1d98ea40007a8afb61
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-05 18:22:44 -0700

    added annotations for IGrabber and GrabberTool, removed debug line

---
+++ toolbox/i_grabber.lua
@@ -0,0 +1,8 @@
+--- @meta
+--- Provides access to the grabber tool's properties.
+--- 
+--- Only available if the builder tool's ID is 4.
+--- @class IGrabber
+--- @field IsGrabbing boolean `true` if the grabber tool is currently grabbing something; otherwise, `false`.
+--- @field IsShoving boolean `true` if the grabber tool is currently shoving something; otherwise, `false`.
+local IGrabber = {}
