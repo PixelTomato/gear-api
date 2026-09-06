06818dc69b50dd2d752b79d74e63405ecffed3c8
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-05 20:37:49 -0700

    added annotations for IImage

---
+++ ui/iimage.lua
@@ -0,0 +1,7 @@
+--- @meta
+--- Provides access to the properties of a UI image.
+--- @class IImage
+--- @field Texture Texture The texture displayed by this image.
+--- @field UVRect Rect The UV rect used by this image's texture.
+--- @field Colour Colour The color tint applied to this image.
+local IImage = {}
