71b4dd953fe268498455588aae241c88e4eb7c6d
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-06 02:04:15 -0700

    added annotations for IMaterialApplier and MaterialTool

---
+++ toolbox/imaterialapplier.lua
@@ -0,0 +1,7 @@
+--- @meta
+--- Provides access to the material tool's properties.
+--- 
+--- Only available if the builder tool's ID is 1.
+--- @class IMaterialApplier
+--- @field IsApplying boolean `true` if the material tool is applying a material; otherwise, `false`.
+local IMaterialApplier = {}
