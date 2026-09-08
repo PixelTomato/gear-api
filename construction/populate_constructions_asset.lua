1e7396c770b70c90e8c68bb63a8362a8d3bfb12e
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-08 15:37:36 -0700

    added annotations for PopulateConstructionsAsset

---
+++ construction/populate_constructions_asset.lua
@@ -0,0 +1,13 @@
+--- @meta
+--- Populate constructions.
+--- 
+--- Static methods are available via the `PopConstructions` global.
+--- 
+--- Example:
+--- ```lua
+--- PopConstructions.SpawnPart(AssetGUID.__new("Propeller 3Blade"))
+--- ```
+--- @class PopulateConstructionsAssets : IPopulateConstructions
+PopConstructions = {}
+
+--- @alias PopConstructions PopulateConstructionsAssets
