f682a42935f29dc15876dc7017321526bbc654e7
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-09 14:57:26 -0700

    added annotations for IProfilerMetricRecorder

---
+++ profiling/i_profiler_metric_recorder.lua
@@ -0,0 +1,9 @@
+--- @meta
+--- Provides access to a profiler metric recorder's properties.
+--- @class IProfilerMetricRecorder
+--- @field Category string The name of the profiler metric category.
+--- @field Name string The name of the profiler metric.
+--- @field ValueAsLong number The metric value as a long integer.
+--- @field ValueAsDouble number The metric value as a double precision float.
+--- @field ValueAsString string The metric value as a string.
+local IProfilerMetricRecorder = {}
