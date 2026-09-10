e09c402292e67d071212ba7bf807b772cc33614a
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-09 20:04:10 -0700

    added annotations for IScrollView

---
+++ ui/i_scroll_view.lua
@@ -0,0 +1,7 @@
+--- @meta
+--- Provides access to a UI scroll view's properties.
+--- @class IScrollView
+--- @field ContainerSize number The size of this scroll view's container.
+--- @field AutoResize boolean Set to `true` to automatically resize the container; otherwise, `false`.
+--- @field ScrollPos number The scroll position of this scroll view.
+local IScrollView = {}
