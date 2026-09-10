2fe99074c968a04175a5fb6331e32d42a6dd9226
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-10 01:38:05 -0700

    added annotations for ITargeter

---
+++ toolbox/i_targeter.lua
@@ -0,0 +1,8 @@
+--- @meta
+--- Provides access to a player's targeter.
+--- @class ITargeter : INullCheck
+--- @field TargetedPart IPart The targeted part, if any.
+--- @field TargetedPosition Vector3 The position on the targeted part, in world coordinates.
+--- @field IsTargetingPart boolean `true` if the player is targeting a part; otherwise `false`.
+--- @field IsTargetingAnything boolean `true` if the player is targeting anything (a part, attachment, link, etc.); otherwise, `false`.
+local ITargeter = {}
