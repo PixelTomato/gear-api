fc2b331ca19d5ee7bd9f8094b7d330d3295f436c
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-10 02:19:40 -0700

    added annotations for IToolbox

---
+++ toolbox/i_toolbox.lua
@@ -0,0 +1,7 @@
+--- @meta
+--- Provides access to a player's toolbox.
+--- @class IToolbox : INullCheck
+--- @field ActiveToolID number The ID of this toolbox's active tool, if any; otherwise, `nil`.
+--- @field ActiveTool ITool This toolbox's active tool, if any; otherwise, `nil`.
+--- @field PartSelection IPartSelection This toolbox's part selection.
+local IToolbox = {}
