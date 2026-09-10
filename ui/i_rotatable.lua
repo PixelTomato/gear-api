b1b2008e9a71327effa9b29c0b10c969c2cd5dc5
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-09 20:01:34 -0700

    added annotations for IRotatable

---
+++ ui/i_rotatable.lua
@@ -0,0 +1,8 @@
+--- @meta
+--- A rotatable UI element.
+--- @class IRotatable
+local IRotatable = {}
+
+--- Set the element's rotation.
+--- @param angle number The rotation angle.
+function IRotatable.SetRotation(angle) end
