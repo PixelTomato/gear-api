e99ac74e0c35b0ec1cd15d8d9effd1680a7d2793
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-09 20:42:09 -0700

    added annotations for ISizeable

---
+++ ui/i_sizeable.lua
@@ -0,0 +1,10 @@
+--- @meta
+--- A sizable UI element.
+--- @class ISizeable
+local ISizeable = {}
+
+--- Set the element's alignment.
+--- @param align Align The alignment mode.
+--- @param paramA number The first parameter.
+--- @param paramB number The second parameter.
+function ISizeable.SetAlignment(align, paramA, paramB) end
