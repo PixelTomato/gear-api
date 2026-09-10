792a0dd59af867d18b944873963af7f430f9b8b2
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-09 20:45:09 -0700

    added annotations for ISlider

---
+++ ui/i_slider.lua
@@ -0,0 +1,9 @@
+--- @meta
+--- Provides access to a UI slider's properties.
+--- @class ISlider
+--- @field Value number The value of this slider.
+--- @field MaxValue number The maximum value this slider allows.
+--- @field MinValue number The minimum value this slider allows.
+--- @field WholeNumbers boolean `true` if this slider should snap to integer values; otherwise, `false`.
+--- @field ValueDisplayFormat string The format string used by this slider for showing its value as text.
+local ISlider = {}
