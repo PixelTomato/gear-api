0f25858049cb7b464e0ef6377045347ea3b91de2
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-11 02:56:53 -0700

    reworked video annotations after making proxy type public

--- scripting/video_player_proxy.lua
+++ scripting/video_player_proxy.lua
@@ -8,18 +8,20 @@
 --- @field IsPaused boolean `true` if this video player is paused; otherwise, `false`.
 --- @field Width number The width of this video player.
 --- @field Height number The height of this video player.
-local VideoPlayerProxy = {}
+local VideoPlayer = {}
 
 --- Load a video.
 --- @param filePath string The file to load the video from.
 --- @param enableAudio boolean Set to `true` to enable audio playback; otherwise, `false`.
-function VideoPlayerProxy.Load(filePath, enableAudio) end
+function VideoPlayer.Load(filePath, enableAudio) end
 
 --- Play the video.
-function VideoPlayerProxy.Play() end
+function VideoPlayer.Play() end
 
 --- Pause the video.
-function VideoPlayerProxy.Pause() end
+function VideoPlayer.Pause() end
 
 --- Stop the video.
-function VideoPlayerProxy.Stop() end
+function VideoPlayer.Stop() end
+
+--- @alias VideoPlayer VideoPlayerProxy
