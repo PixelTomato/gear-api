110dfae2a613fc420f6e3502c8d96b5fd9ceaa43
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-09-17 01:47:47 -0700

    added additional miscellaneous globals

--- misc.lua
+++ misc.lua
@@ -81,7 +81,7 @@ NoPartSpawnDestroy = nil
 --- @type boolean Whether the tool UI overlays are enabled. Defaults to **true**.
 ToolOverlaysEnabled = nil
 
---- @type number The ID of the local player. Defaults to **0**. Must be an integer between **0** and **255** (inclusive).
+--- @type integer The ID of the local player. Defaults to **0**. Must be between **0** and **255** (inclusive).
 LocalPlayerNetworkID = nil
 
 --- @type boolean Whether the center of mass indicator is being shown. Defaults to **true**.
@@ -127,3 +127,89 @@ HelpOverlayEnabled = nil
 
 --- @type boolean Whether energy consumption is enabled. Defaults to **false**.
 EnergyEnabled = nil
+
+--- @type number The save type of the current player character. Defaults to **255** for the default character.
+---
+--- type | description
+--- :--: | :-
+--- 0    | saved locally
+--- 1    | shipped with game
+--- 2    | downloaded from workshop
+PlayerCharacterSaveType = nil
+
+--- @type boolean Whether composite bounds rendering (red outlines around constructions) is enabled. Defaults to **true**.
+ShowCompositeBounds = nil
+
+--- @type number The detection mode for contact between parts. Defaults to **0** (continuous).
+---
+--- mode | description
+--- :--: | :-
+--- 0    | continuous detection
+--- 1    | impact detection only (prevents sliding sounds)
+--- 2    | off (no impact sounds or damage)
+ContactDetectionMode = nil
+
+--- @type number The time delta between simulation fixed updates. Defaults to **0.005**.
+---
+--- This is effectively the same as the "Update Rate" setting in the scene tool. *FixedDeltaTime = 1 / Update Rate*.
+FixedDeltaTime = nil
+
+--- @type Colour The color currently selected in the paint tool. Color defaults to **(0, 0, 0, 255)**.
+SelectedPaintColour = nil
+
+--- @type number The rate the game sends inputs to the server. Defaults to **30**.
+ClientInputSendRate = nil
+
+--- @type Player The local player being controlled.
+LocalPlayer = nil
+
+--- @type number The distance over which object highlights fade out. Defaults to **30**.
+ObjectHighlighterFadeDist = nil
+
+--- @type number The size scale applied to attachments (indicators, etc.). Defaults to **1.0**.
+AttachmentScale = nil
+
+--- @type boolean Whether contructions are aligned to the targeted part when freezed using a tool. Defaults to **true**.
+---
+--- When this is disabled, the construction will be aligned relative to its largest composite instead.
+FreezeAlignToTargetedPart = nil
+
+--- @type boolean Whether the player's movement is disabled. Defaults to **false**.
+LocalPlayerMovementDisabled = nil
+
+--- @type boolean Whether the ability to save the scene is disabled. Defaults to **false**.
+NoSceneSave = nil
+
+--- @type number The main player camera's field of view.
+FieldOfView = nil
+
+--- @type boolean Whether the backgrounds of UIs has a blur effect. Defaults to **true**.
+UIBlurEnabled = nil
+
+--- @type number The height of the environment's water level. Defaults to **0.0`**.
+---
+--- This option has no effect in invironments without water.
+WaterLevel = nil
+
+--- @type number The rate at which the time-of-day progresses. Defaults to **0.0**.
+---
+--- A rate of **0.0** will freeze the time of day. **1.0** allows for normal time. Values above **1.0** are faster than normal.
+TODProgressRate = nil
+
+--- @type integer The number of physics solver position iterations per fixed update. Defaults to **60**.
+---
+--- This value can also be set using the "Update Rate" and "Constraint Accuracy" sliders in the simulation settings.'
+--- It seems to be calculated as **CU⁻² × 10⁶** where C is constraint accuracy and U is update rate.
+SolverPositionIterations = nil
+
+--- @type number The number of physics solver velocity iterations per fixed update. Defaults to **1**.
+SolverVelocityIterations = nil
+
+--- @type number The current acceleration applied do to gravity, in m/s². Defaults to **9.81**.
+Gravity = nil
+
+--- @type boolean Whether constructions will be shown on the compass. Defaults to **true**.
+ShowAllConstructionsOnCompass = nil
+
+--- @type number The size of the rotation step for selected objects, in degrees. Defaults to **90**.
+SelectionStepAngle = nil
